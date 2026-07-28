8e91eac14ed7dc791124a03c835b836bf15adfda
miywa1310 <miywadoshmuratqizy@gmail.com>
2026-07-28 12:44:07 +0500

    added 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://furniq-api.onrender.com/";
+const BASE_URL = "https://furniq-api.onrender.com";
 
 export const api = axios.create({
   baseURL: BASE_URL,
